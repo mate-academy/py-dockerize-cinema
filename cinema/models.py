b967d2651a718502e7aa413cc0cea45a04901c90
danylott <d.tuytuyshkin@gmail.com>
2022-06-15 17:21:15 +0300

    Fix `.` in extension

--- cinema/models.py
+++ cinema/models.py
@@ -41,7 +41,7 @@ class Actor(models.Model):
 
 def movie_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}-{uuid.uuid4()}.{extension}"
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
     return os.path.join("uploads/movies/", filename)
 
