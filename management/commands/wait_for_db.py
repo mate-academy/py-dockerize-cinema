9fda3b2fc654bc2ed1f0a958e967b047e9e49be8
Inna Kushnir <innakushnir63@gmail.com>
2022-12-29 00:02:21 +0200

    Solution

--- management/commands/wait_for_db.py
+++ management/commands/wait_for_db.py
@@ -5,16 +5,17 @@ import time
 
 
 class Command(BaseCommand):
-    """ Django command to pause execution until database is available"""
+    """Django command to pause execution until database is available"""
+
     def handle(self, *args, **kwargs):
-        self.stdout.write('waiting for db ...')
+        self.stdout.write("waiting for db ...")
         db_conn = None
         while not db_conn:
             try:
                 # get the database with keyword 'default' from settings.py
-                db_conn = connections['default']
+                db_conn = connections["default"]
                 # prints success message in green
-                self.stdout.write(self.style.SUCCESS('db available'))
+                self.stdout.write(self.style.SUCCESS("db available"))
             except OperationalError:
                 self.stdout.write("Database unavailable, waiting 1 second ...")
 
