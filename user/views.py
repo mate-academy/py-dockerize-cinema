40b709ad8470b2729b228ae16e0b5862eeb97870
Danylo Tiutiushkin <48014331+danylott@users.noreply.github.com>
2023-01-24 12:05:38 +0200

    Fixed Token auth -> JWT auth for user/me endpoint

--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer
 
@@ -11,7 +11,7 @@ class CreateUserView(generics.CreateAPIView):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
+    authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
