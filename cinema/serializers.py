f40941a7d4864ba122d6e2c3270c7168dc5b26b1
Danylo Tiutiushkin <48014331+danylott@users.noreply.github.com>
2023-01-26 11:00:15 +0200

    Fixed ValidationError

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,5 +1,6 @@
 from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from cinema.models import (
     Genre,
@@ -113,7 +114,7 @@ class TicketSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         data = super(TicketSerializer, self).validate(attrs=attrs)
         Ticket.validate_ticket(
-            attrs["row"], attrs["seat"], attrs["movie_session"]
+            attrs["row"], attrs["seat"], attrs["movie_session"], ValidationError
         )
         return data
 
