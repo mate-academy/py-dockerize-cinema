53c10996249456b3e29599084fa216290fc45e02
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-01-27 19:48:41 +0200

    'Solution'

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -117,7 +117,6 @@ class TicketSerializer(serializers.ModelSerializer):
             attrs["row"],
             attrs["seat"],
             attrs["movie_session"],
-            ValidationError
         )
         return data
 
