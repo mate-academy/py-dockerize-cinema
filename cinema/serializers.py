9480574e27a20ea9ebedca3028ebd7cb12788557
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-01-27 19:53:59 +0200

    'Solution'

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,6 @@
-from django.core.exceptions import ValidationError
 from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from cinema.models import (
     Genre,
@@ -117,6 +117,7 @@ class TicketSerializer(serializers.ModelSerializer):
             attrs["row"],
             attrs["seat"],
             attrs["movie_session"],
+            ValidationError,
         )
         return data
 
