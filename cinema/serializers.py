292884ffabb60d16e7b3f5c6623a9e2f61aa9dae
eugene <bantyc8@gmail.com>
2023-01-29 11:41:53 +0200

    Add blank

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -114,7 +114,9 @@ class TicketSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         data = super(TicketSerializer, self).validate(attrs=attrs)
         Ticket.validate_ticket(
-            attrs["row"], attrs["seat"], attrs["movie_session"],
+            attrs["row"],
+            attrs["seat"],
+            attrs["movie_session"],
             ValidationError
         )
         return data
