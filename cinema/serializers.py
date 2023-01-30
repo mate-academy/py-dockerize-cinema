3fae7dd82a26edc57de0a0e5d71b94ac8c193c75
Danylo Tiutiushkin <48014331+danylott@users.noreply.github.com>
2023-01-30 13:45:06 +0200

    Update serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -114,7 +114,10 @@ class TicketSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         data = super(TicketSerializer, self).validate(attrs=attrs)
         Ticket.validate_ticket(
-            attrs["row"], attrs["seat"], attrs["movie_session"], ValidationError
+            attrs["row"], 
+            attrs["seat"], 
+            attrs["movie_session"], 
+            ValidationError
         )
         return data
 
