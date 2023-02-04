700723df3fadad98c11fb137c0c9e8ea5e41cb51
V K <kvmltkv@gmail.com>
2023-02-04 16:13:23 +0200

    Finding whitespaces for flake8

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -111,6 +111,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    """For commit to find whitespaces"""
     def validate(self, attrs):
         data = super(TicketSerializer, self).validate(attrs=attrs)
         Ticket.validate_ticket(
