e32afe9391f57b7891bb7f4d44d01580a9340fc2
V K <kvmltkv@gmail.com>
2023-02-04 14:00:20 +0200

    flake8 is crazy

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,5 +1,6 @@
 from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from cinema.models import (
     Genre,
@@ -115,7 +116,8 @@ class TicketSerializer(serializers.ModelSerializer):
         Ticket.validate_ticket(
             attrs["row"],
             attrs["seat"],
-            attrs["movie_session"]
+            attrs["movie_session"],
+            ValidationError
         )
         return data
 
