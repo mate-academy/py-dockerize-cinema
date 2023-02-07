347fb0a2c44a123fa119e60d21c12c0e526d5f80
Vlad Hrytsai <sunrises2929@gmail.com>
2023-02-07 17:03:36 +0200

    solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "HOST": os.environ["POSTGRES_HOST"],
-        "NAME": os.environ["POSTGRES_NAME"],
+        "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
     }
