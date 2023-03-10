54965bd4637f135af6f246db9c2f131577034a49
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-03-10 15:14:29 +0200

    Add typehint to the handle method in wait_for_db.py

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -1,4 +1,6 @@
 import time
+from typing import Any
+
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management import BaseCommand
@@ -7,7 +9,7 @@ from django.core.management import BaseCommand
 class Command(BaseCommand):
     """Django command to pause execution until db is available"""
 
-    def handle(self, *args, **options):
+    def handle(self, *args: Any, **options: Any) -> None:
         self.stdout.write("Waiting for database...")
         db_conn = None
         while not db_conn:
