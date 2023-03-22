1aad660ad974c89a80391f2b39d5c8f184d0fd72
Paul Starodub <amorallex@gmail.com>
2023-03-22 17:20:15 +0400

    Cinema

---
+++ cinema/mangement/commands/wait_for_db.py
@@ -0,0 +1,20 @@
+import time
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to pause execution until db is available"""
+
+    def handle(self, *args: tuple, **options: dict) -> None:
+        self.stdout.write("Waiting for database...")
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections["default"].cursor()
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
