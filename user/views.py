ac4da36163aaaad72a87ca918ecf8fa7ae00c7fc
Paul Starodub <amorallex@gmail.com>
2023-03-22 18:02:18 +0400

    Solo

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.models import User
@@ -7,7 +7,6 @@ from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
-    # queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
