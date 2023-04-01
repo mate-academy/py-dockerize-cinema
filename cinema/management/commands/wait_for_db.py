3eaf5d7fb1f8beff9a312a63ac726d015e29e206
Іван Березянський <mamanadarin@gmail.com>
2023-04-01 16:05:31 +0300

    Fix

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -10,12 +10,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         port = int(os.environ["POSTGRES_PORT"])
+        db_host = os.environ["POSTGRES_HOST"]
         db_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         while True:
             try:
-                db_socket.connect(("db", port))
+                db_socket.connect((db_host, port))
                 db_socket.close()
                 break
             except socket.error:
-                time.sleep(0.1)
+                time.sleep(1)
