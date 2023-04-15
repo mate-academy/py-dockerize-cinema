4d29e3aaaa55aa11b645a694e125b662ef837378
Zoriana Dvulit <zdvulit@ukr.net>
2023-04-15 15:59:17 +0300

    Removing commented code in settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -88,13 +88,11 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
         "ENGINE": "django.db.backends.postgresql",
         "HOST": os.environ["DB_HOST"],
         "NAME": os.environ["DB_NAME"],
         "USER": os.environ["DB_USER"],
         "PASSWORD": os.environ["DB_PASSWORD"],
-        # "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
