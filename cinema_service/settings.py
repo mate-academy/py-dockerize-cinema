491217df5c1c52bedf10be2f9b14e84640ea8123
oithegod <serejathegod@gmail.com>
2023-04-20 16:35:41 +0300

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR/".env")
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
