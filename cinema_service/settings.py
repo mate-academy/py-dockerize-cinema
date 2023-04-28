638d2690679188c2afb6ad3bdb633ebafca52e54
ostapix@gmail.com <@Bandera345>
2023-04-28 16:39:49 +0300

    Change enviroment path to .env file in docker-compose.yml

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -88,10 +88,10 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ["DB_HOST"],
-        "NAME": os.environ["DB_NAME"],
-        "USER": os.environ["DB_USER"],
-        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
     }
 }
 
