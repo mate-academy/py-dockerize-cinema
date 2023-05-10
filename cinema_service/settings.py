55cc5781a68aa6409d0fc552642720c630d7b26b
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-10 11:44:56 +0300

    Add all db credentials temporarily in settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -85,13 +85,23 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "HOST": os.environ["DB_HOST"],
+#         "NAME": os.environ["DB_NAME"],
+#         "USER": os.environ["DB_USER"],
+#         "PASSWORD": os.environ["DB_PASSWORD"],
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ["DB_HOST"],
-        "NAME": os.environ["DB_NAME"],
-        "USER": os.environ["DB_USER"],
-        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": "balarama.db.elephantsql.com",
+        "NAME": "msyarjsn",
+        "USER": "msyarjsn",
+        "PASSWORD": "uSF7tNLJDko6MFTFg14a0l6FUtJ0IEHg",
     }
 }
 
