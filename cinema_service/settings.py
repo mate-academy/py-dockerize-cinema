52e829ab83208dadb25242214db63adddca4f382
Andriy <andriykrav@gmail.com>
2023-05-21 21:44:37 +0300

    correct some notices

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -101,24 +101,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'cinemadb',
-#         'USER': 'cinemadb',
-#         'PASSWORD': 'cinemadb',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
