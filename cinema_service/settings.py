306cf5b5dca3b2c8e7b80c905c69d97b4da7441e
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-13 14:59:52 +0300

    upd TIME_ZONE && set USE_TZ=True && upd STATIC_ROOT & MEDIA_ROOT

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -130,20 +130,21 @@ AUTH_USER_MODEL = "user.User"
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = "static"
+MEDIA_ROOT = "media"
 
-STATIC_URL = "static/"
-
+STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
