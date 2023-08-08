655d1ba613af0adf1961643abba5d6c21f8ccb2f
oleksiikolii <igravkompik@gmail.com>
2023-08-08 16:51:49 +0300

    flake fix

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -5,7 +5,6 @@ from django.db import connections, OperationalError
 
 
 class Command(BaseCommand):
-    help = "Wait for db setting up and prevents app from running without db, timeout 15 sec"
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("Waiting for connection"))
