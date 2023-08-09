8b3383cbe2ec90e42a33129d936c6dacaf77e300
Eugene Polunin <altahoma@gmail.com>
2023-08-09 13:03:44 +0300

    Fixed flake8 errors

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -6,17 +6,15 @@ from django.db.utils import OperationalError
 
 
 class Command(BaseCommand):
-    help = 'Waits for the database to be available before proceeding'
-
     def handle(self, *args, **options):
-        self.stdout.write('Waiting for database...')
+        self.stdout.write("Waiting for database...")
         db_conn = None
         while not db_conn:
             try:
                 connection.ensure_connection()
                 db_conn = True
             except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+        self.stdout.write(self.style.SUCCESS("Database available!"))
