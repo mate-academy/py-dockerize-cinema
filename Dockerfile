6800bf09a811fd892474c681273d47aa3398ad57
andy77andy <andy77andy@ukr.net>
2023-08-09 16:07:32 +0300

    Solution

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,20 @@ LABEL maintainer="andy77andy77andy@gmail.com"
 ENV PYTHONUNBUFFERED 1
 WORKDIR app/
 
-#RUN apt-get update\
-#    && apt-get -y install libpq-dev gcc
+RUN apt-get update\
+    && apt-get -y install libpq-dev gcc
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 COPY . .
+
+RUN mkdir -p /vol/web/media
 
 RUN adduser \
         --disabled-password \
         --no-create-home \
         django-user
 
+RUN chown -R django-user:django-user /vol/
+RUN chmod -R 755 /vol/web
 USER django-user
--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-#from dotenv import load_dotenv
-#load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,7 +90,7 @@ DATABASES = {
 
         "ENGINE": "django.db.backends.postgresql",
         "HOST": os.environ["POSTGRES_HOST"],
-        "NAME": os.environ["POSTGRES_NAME"],
+        "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"]
     }
@@ -137,7 +137,7 @@ USE_TZ = False
 STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/vol/web/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
