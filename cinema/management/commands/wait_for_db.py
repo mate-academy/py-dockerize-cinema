9335c5b896108938788fd1621db043b0ea6e34f4
anna-lybid <anna.lybid@gmail.com>
2023-10-24 21:07:42 +0300

    Flake update

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -4,8 +4,9 @@ from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-    help = "Wait until the database is available"
+    help_ = "Wait until the database is available"
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.NOTICE("Waiting for database..."))
@@ -14,6 +15,10 @@ class Command(BaseCommand):
             try:
                 db_conn = connections["default"]
             except OperationalError:
-                self.stdout.write(self.style.NOTICE("Database unavailable, waiting 1 second..."))
+                self.stdout.write(
+                    self.style.NOTICE(
+                        "Database unavailable, waiting 1 second..."
+                    )
+                )
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS("Database available!"))
