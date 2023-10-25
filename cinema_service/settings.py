2909067f28427312df7f40999d0b77956046813b
IvanGuculyak <ivan.guculyak@gmail.com>
2023-10-25 20:04:11 +0300

    solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -86,10 +86,6 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "HOST": os.environ["POSTGRES_HOST"],
