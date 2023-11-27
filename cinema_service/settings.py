7747daeb93132411395ee94dfe4933a9f2d744ff
Sergii Pryima <spryima@gmail.com>
2023-11-27 09:51:46 +0200

    solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -171,7 +171,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=25),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
