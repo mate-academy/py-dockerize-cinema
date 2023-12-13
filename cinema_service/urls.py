0f97fa6ba2a139fc33c5cf9ba8d7e5e1a51af53a
Roman Zhadan <21gehassig@gmail.com>
2023-12-13 05:38:17 +0200

    'Solution'

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path(
         "api/doc/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
+        name="swagger-uцi",
     ),
     path(
         "api/doc/redoc/",
