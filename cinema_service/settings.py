03b01fb5fe939b0c8c312b932067d1b557542c78
Roman Zhadan <21gehassig@gmail.com>
2023-12-14 02:33:56 +0200

    change duration for throttle to normal

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ],
-    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "300/day"},
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/day", "user": "30/day"},
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
