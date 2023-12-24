a18d8c06348c699db193f6d33726a3b7aa6a64d0
Vitalii <vitalikkhmura132gmail.com>
2023-12-24 18:45:34 +0200

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +87,8 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+print(os.environ["POSTGRES_HOST"])
 
 DATABASES = {
     "default": {
