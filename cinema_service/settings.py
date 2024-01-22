bf77f411282ceb1c52fe22b3cb0d557518c00d3d
nsdNite <nsd7night@gmail.com>
2024-01-22 13:46:28 +0200

    Minor settings.py update

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -142,8 +142,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-MEDIA_URL = "/media/"
 MEDIA_ROOT = "/vol/web/media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
