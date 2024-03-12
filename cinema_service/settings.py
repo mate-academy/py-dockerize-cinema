9ffa49432fb30180293aa6e35f87c57d1c1bc3a1
Viktor Deyneka <3lobites@gmail.com>
2024-03-12 20:41:51 +0200

    added wait_for_db, fixed commit for flake8

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -15,12 +15,12 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), "..\.env")
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 # dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-# mmm = os.environ["DP_PASSWORD"]
+mmm = os.environ["DP_PASSWORD"]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
