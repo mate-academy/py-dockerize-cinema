8595382bcd59913a30118d689b217b8f2f692627
Kontroler444 <kontroler444@gmail.com>
2024-03-19 13:39:53 +0200

    Dockerize DRF Cinema

---
+++ cinema/management/commands/wait_for_db.py
@@ -0,0 +1,32 @@
+import sys
+import time
+
+from django.core.management.base import BaseCommand
+from django.db import connection
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    help = "Wait for db"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--max_retries", type=int, default=60)
+
+    def handle(self, *args, **options):
+        max_retries = options["max_retries"]
+
+        for retry in range(max_retries):
+            try:
+                connection.ensure_connection()
+                self.stdout.write(self.style.SUCCESS("Connection Success"))
+            except OperationalError as e:
+                self.stdout.write(
+                    f"Database unavailable on attempt {retry+1}/{max_retries}: {e}"
+                )
+                time.sleep(3)
+            else:
+                break
+        else:
+            self.stdout.write(self.style.ERROR("Database unavailable"))
+            sys.exit(1)
+
