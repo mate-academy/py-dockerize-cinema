2946771b16060e0bb898cad455cada2fb9fb3bd1
Mykola <mykola22dubyna@gmail.com>
2024-03-27 09:01:50 +0200

    fix wait for db command

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -1,21 +1,20 @@
-from time import sleep
+import socket
 
 from django.core.management.base import BaseCommand, CommandError
-
-from cinema.models import Genre
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        for _ in range(5):
-            try:
-                Genre.objects.all()
-                self.stdout.write(
-                    self.style.SUCCESS("DB runs successfully")
-                )
-                return
-            except Genre.DoesNotExist:
-                sleep(1)
+        print("Waiting for DB")
+        try:
+            check_socket = socket.create_connection(("db", 5432), 5)
+            check_socket.close()
+            self.stdout.write(
+                self.style.SUCCESS("DB runs successfully")
+            )
+            return
+        except socket.error as e:
+            print(e)
 
         raise CommandError("DB doesn't exist")
