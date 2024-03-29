3d7277bb8ba16662f758c56c55beda1d9e12f711
Maksym Operchuk <operlast529@gmail.com>
2024-03-29 19:53:35 +0200

    Solution

--- cinema/management/commands/waiter.py
+++ cinema/management/commands/waiter.py
@@ -1,9 +1,19 @@
-from django.core.management.base import BaseCommand
-from time import sleep
+import time
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write("Waiting for database...")
-        sleep(1)
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections["default"]
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
