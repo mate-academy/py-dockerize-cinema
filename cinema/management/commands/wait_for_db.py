d2e753469d31ddeab1a4b864c5df92e9cdb37d11
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-03-31 14:04:03 +0300

    fixed command and flake8

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -9,12 +9,12 @@ class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **kwargs):
-        self.stdout.write('Waiting for database...')
+        self.stdout.write("Waiting for database...")
         db_conn = None
         while not db_conn:
             try:
-                db_conn = connections['default']
+                db_conn = connections["default"]
             except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS('Database available'))
+        self.stdout.write(self.style.SUCCESS("Database available"))
