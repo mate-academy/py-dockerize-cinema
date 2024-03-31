fc30900470f713b8ba1029c8e7f665bc0d2dac5a
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-03-31 14:01:57 +0300

    fixed command and flake8

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -1,15 +1,20 @@
-from time import sleep
+import time
 
+from django.db import connections
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    helps = "Make program sleep for n seconds"
+    """Django command to pause execution until database is available"""
 
-    def add_arguments(self, parser):
-        parser.add_argument("seconds", type=int)
-
-    def handle(self, *args, **options):
-        seconds = options.get("seconds")
-        sleep(seconds)
-        print(f"Waiting {seconds} seconds ...")
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Waiting for database...')
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections['default']
+            except OperationalError:
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database available'))
