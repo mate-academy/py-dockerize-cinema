76e00039d9f919b526df76afe1b0ca9c1a08129e
Molchanov Ihor <=>
2024-04-13 14:22:11 +0300

    fix media folder root

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "/vol/djangofiles/media"
+MEDIA_ROOT = "/vol/web/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
