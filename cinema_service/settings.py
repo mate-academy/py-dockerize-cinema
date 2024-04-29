bcf8a857e4df833e8d83ce2704fa6b96867000c0
Anton Tkachuk <tkacuk2291@gmail.com>
2024-04-29 19:33:01 +0300

    solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -91,13 +91,7 @@ DATABASES = {
         "PORT": os.environ["POSTGRES_PORT"],
     }
 }
-#
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
