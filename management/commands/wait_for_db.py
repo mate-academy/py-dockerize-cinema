387c27485898454227cbd15a64408366eebbbd62
MaksymUK <clear>
2024-05-01 08:16:27 +0100

    change wait_for_db.py method

--- management/commands/wait_for_db.py
+++ management/commands/wait_for_db.py
@@ -1,22 +1,19 @@
 from datetime import time
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+from django.db import connections, OperationalError
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.stdout.write("Waiting for database")
+        self.stdout.write("Waiting for database...")
         db = None
-        try:
-            while db is None:
-                try:
-                    db = self.check(databases=["default"])
-                except Exception:
-                    self.stdout.write(
-                        "Database unavailable, waiting 1 second..."
-                    )
-                    time.sleep(1)
-            self.stdout.write(self.style.SUCCESS("Database available!"))
-        except Exception as e:
-            raise CommandError(str(e))
+        while not db:
+            try:
+                db = connections["default"]
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
