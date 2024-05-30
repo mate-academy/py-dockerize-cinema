85b71bd36ed880f0968da49806db715322762002
ilgan <2ilgan1601@gmail.com>
2024-05-30 23:28:45 +0300

    4

--- management/commands/wait_for_db.py
+++ management/commands/wait_for_db.py
@@ -6,9 +6,12 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        self.stdout.write("Database connection")
         db_connection = None
         while not db_connection:
             try:
                 db_connection = connections["default"]
+                self.stdout.write(self.style.SUCCESS("Connect"))
             except OperationalError:
+                self.stdout.write("Error, retry after 3 seconds")
                 time.sleep(3)
