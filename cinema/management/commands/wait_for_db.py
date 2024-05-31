105fc4d7ad709fd1833ed375efd71373bc18128c
Alexander <sasacihun@gmail.com>
2024-05-31 17:48:31 +0300

    fix: Fix flake8 error ( variable names that shadow builtins are not allowed). Redefine `help` variable

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -13,7 +13,7 @@ WAITING = 1
 
 
 class Command(BaseCommand):
-    help = "Waits for the database to be available"
+    help_prevent_shadow_builtins = "Waits for the database to be available"
 
     def handle(self, *args, **options):
         self.stdout.write("Waiting for database...")
@@ -26,3 +26,22 @@ class Command(BaseCommand):
                 self.stdout.write(f"Database unavailable, waiting"
                                   f" {WAITING} second...")
                 time.sleep(WAITING)
+
+    def create_parser(self, prog_name, subcommand, **kwargs):
+        """
+        Create and return the ``ArgumentParser`` which will be used to
+        parse the arguments to this command.
+        """
+        parser = CommandParser(
+            prog="%s %s" % (os.path.basename(prog_name), subcommand),
+            description=self.help_prevent_shadow_builtins or None,
+            formatter_class=DjangoHelpFormatter,
+            missing_args_message=getattr(self, "missing_args_message", None),
+            called_from_command_line=getattr(
+                self,
+                "_called_from_command_line",
+                None
+            ),
+            **kwargs,
+        )
+        return parser
