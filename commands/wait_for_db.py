033242af5be469af376caee49603bd15d9cc4a65
Oleksandr Moroz <molecxander@gmail.com>
2024-06-06 20:23:20 +0300

    Solution

--- commands/wait_for_db.py
+++ commands/wait_for_db.py
@@ -5,8 +5,6 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Wait for the database to be available"
-
     def handle(self, *args, **options):
         self.stdout.write("Waiting for database...")
         db_conn = None
