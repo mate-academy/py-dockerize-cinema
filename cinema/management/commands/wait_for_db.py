3986d1d1f92863b71fda2b980ba2ef2eef0285e6
Gibbs <mr.darmstadtiumgmail.com>
2024-06-23 10:46:18 +0200

    pass all tests

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -5,7 +5,6 @@ from django.db.utils import OperationalError
 
 
 class Command(BaseCommand):
-    help = "Wait for the database to be available"
 
     def handle(self, *args, **options):
         self.stdout.write("Waiting for database...")
