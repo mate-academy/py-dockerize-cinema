38139d998b6918c74306fc146182c55527d1e6b9
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-06-25 16:22:07 +0300

    fixed flake8

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -8,12 +8,12 @@ class Command(BaseCommand):
     help = "Your app waits for the database to be available"
 
     def handle(self, *args, **kwargs):
-        self.stdout.write('Waiting for database...')
+        self.stdout.write("Waiting for database...")
         db_conn = None
         while not db_conn:
             try:
-                db_conn = connections['default']
-                self.stdout.write(self.style.SUCCESS('Database available!'))
+                db_conn = connections["default"]
+                self.stdout.write(self.style.SUCCESS("Database available!"))
             except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
