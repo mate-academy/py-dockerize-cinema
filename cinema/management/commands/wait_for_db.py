8c2a316a1eae6a94bf70c8214064fde3a08f438d
MykolaMazhula <mykm3ua@gmail.com>
2024-07-17 19:49:49 +0200

    Create wait_for_db command Use wait_for_db in docker-compose.yaml

---
+++ cinema/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Wait 2 seconds for database container is up'
+
+
+    def handle(self, **kwargs):
+       sleep(2)
