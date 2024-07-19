b72331087894a113c35af0f814ecfb0270f81074
MykolaMazhula <mykm3ua@gmail.com>
2024-07-20 00:05:35 +0200

    Check connection to the database before cinema service starts

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -1,10 +1,29 @@
 from time import sleep
 
 from django.core.management.base import BaseCommand
+from django.db import connection
+from django.db.utils import OperationalError
+
+
+ATTEMPT_NUMBER = 35
+DELAY = 0.15
 
 
 class Command(BaseCommand):
-    help = "Wait 2 seconds for database container is up"
+    help = "Wait for database container is up"
 
     def handle(self, **kwargs):
-        sleep(2)
+        attempt = 0
+        while True:
+            try:
+                cursor = connection.cursor()
+                cursor.close()
+                break
+            except OperationalError:
+                if attempt < ATTEMPT_NUMBER:
+                    attempt += 1
+                    sleep(DELAY)
+                    continue
+                else:
+                    self.stdout.write("Database is not accessable")
+                    break
