e94811cef7246db73ff24fca6556fac267ddfc66
haldaniko <haldaniko@gmail.com>
2024-07-24 14:28:36 +0200

    update Dockerfile, serializers and wait_for_db

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,23 @@
 FROM python:3.11.6-alpine3.18
 LABEL maintainer="haldaniko@gmail.com"
 
-ENV PYTHONNBUFFERED 1
+ENV PYTHONUNBUFFERED=1
 
-WORKDIR app/
+WORKDIR /app
 
 COPY requirements.txt requirements.txt
 
-RUN pip install -r requirements.txt
+RUN apk add --no-cache gcc musl-dev libpq-dev && \
+    pip install --no-cache-dir -r requirements.txt && \
+    apk del gcc musl-dev
 
 COPY . .
 
-RUN mkdir -p /files/media
+RUN mkdir -p /vol/web/media
 
-RUN adduser \
-    --disabled-password \
-    --no-create-home \
-    my_user
+RUN adduser --disabled-password --no-create-home my_user
 
-RUN chown -R my_user /files/media
-RUN chmod -R 755 /files/media
+RUN chown -R my_user /vol/web/media
+RUN chmod -R 755 /vol/web/media
 
 USER my_user
--- cinema/commands/wait_for_db.py
+++ cinema/commands/wait_for_db.py
@@ -1,19 +1,17 @@
-from django.core.management.base import BaseCommand
+import time
 from django.db import connections
 from django.db.utils import OperationalError
-from time import sleep
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write("Waiting for database...")
+        self.stdout.write('Waiting for database...')
         db_conn = None
         while not db_conn:
             try:
-                db_conn = connections["default"]
-                db_conn.cursor()
+                db_conn = connections['default']
+                self.stdout.write(self.style.SUCCESS('Database available!'))
             except OperationalError:
-                self.stdout.write("Database unavailable, waiting...")
-                sleep(1)
-
-        self.stdout.write(self.style.SUCCESS("Database available!"))
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
