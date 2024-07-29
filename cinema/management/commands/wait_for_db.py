6f7987c8a05210b260b7104816f4ca469e3e43a3
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-07-29 21:27:07 +0300

    Add connection to db in  wait_for_db.py

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -1,7 +1,16 @@
+from django.db import connections
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 import time
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        time.sleep(1)
+        db_up = False
+        while not db_up:
+            try:
+                db_conn = connections["default"]
+                db_conn.cursor()
+                db_up = True
+            except OperationalError:
+                time.sleep(1)
