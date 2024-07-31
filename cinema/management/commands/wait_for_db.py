ddf554f6194b9110078660b13542e5d4d44b0c63
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-07-31 14:03:11 +0300

    Edit wait_for_db command - the waiting process is detailed and displayed for the user

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -6,11 +6,23 @@ import time
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        self.stdout.write("Waiting for database...")
         db_up = False
-        while not db_up:
+        attempts = 0
+        max_attempts = 60
+        while not db_up and attempts < max_attempts:
             try:
                 db_conn = connections["default"]
                 db_conn.cursor()
                 db_up = True
             except OperationalError:
+                attempts += 1
+                self.stdout.write(f"Database unavailable, waiting 1 second..."
+                                  f"( attempt {attempts}/{max_attempts})")
                 time.sleep(1)
+
+        if db_up:
+            self.stdout.write(self.style.SUCCESS("Database available!"))
+        else:
+            self.stdout.write(self.style.ERROR("Database not available after"
+                                               " maximum attempts!"))
