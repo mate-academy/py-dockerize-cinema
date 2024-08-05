976d1023ba8ef4c3c26eacffcc5318f674c35a7f
Serhii <Musienko.S.A.1983@gmail.com>
2024-08-05 23:05:22 +0300

    Flake8 fix midtake

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -22,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-6vubhk2$++agnctay_4pxy_8cq)mosmn(*-#2b^v4cgsh-^!i3")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-6vubhk2$++agnctay_4pxy_8cq)mosmn(*-#2b^v4cgsh-^!i3"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
