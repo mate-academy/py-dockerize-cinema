c969313961abb464749ee6d440b3ad656b6f0e33
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-08-06 16:58:24 +0300

    fix flake8 errors

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -5,17 +5,21 @@ import time
 
 
 class Command(BaseCommand):
-    help = 'Checks if the database is ready for connections.'
+    help = "Checks if the database is ready for connections."  # noqa VNE003
 
     def handle(self, *args, **kwargs):
-        db_conn = connections['default']
+        db_conn = connections["default"]
         max_retries = 5
         for i in range(max_retries):
             try:
                 db_conn.cursor()
-                self.stdout.write(self.style.SUCCESS('Database is ready!'))
+                self.stdout.write(self.style.SUCCESS("Database is ready!"))
                 return
             except OperationalError:
-                self.stdout.write(f'Database not ready, retrying {i + 1}/{max_retries}...')
+                self.stdout.write(
+                    f"Database not ready, retrying {i + 1}/{max_retries}..."
+                )
                 time.sleep(2)
-        self.stdout.write(self.style.ERROR('Database not ready after maximum retries.'))
+        self.stdout.write(self.style.ERROR(
+            "Database not ready after maximum retries."
+        ))
