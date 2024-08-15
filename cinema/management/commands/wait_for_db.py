deae7184b2ce5310ea74d0cbb242ec130b7d3a52
Valentin Pozniak <waliker448@gmail.com>
2024-08-15 14:14:56 +0300

    solution4

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -17,4 +17,4 @@ class Command(BaseCommand):
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS("Database available!"))
+        self.stdout.write(self.style.SUCCESS("Database available!")
