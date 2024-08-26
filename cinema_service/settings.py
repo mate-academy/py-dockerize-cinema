e5a2c05a6c40a4e7c9c22962d02f3b0fd9defb26
xborismenx <boris.tikhonov.21@gmail.com>
2024-08-26 22:13:27 +0300

    fixed comments

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -85,12 +85,6 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 DATABASES = {
     "default": {
