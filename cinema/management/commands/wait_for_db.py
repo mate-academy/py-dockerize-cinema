c08836d981fa34f93c4ed421fdfbce57d586f773
sergey <38cerg12021979@jmal.com>
2024-09-12 15:19:36 +0300

    fix files serializers.py & wait_for_db.py

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
