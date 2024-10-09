084d60224a780023cd778279f4d633945a6c3252
serhii <ockar2018@gmail.com>
2024-10-09 18:55:48 +0300

    fix

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -5,26 +5,18 @@ from django.db import connections
 from django.db.utils import OperationalError
 
 
-def log_db_wait(func):
-    def wrapper(self, *args, **kwargs):
-        self.stdout.write("Waiting for database...")
-        result = func(self, *args, **kwargs)
-        self.stdout.write(self.style.SUCCESS("Database available!"))
-        return result
-    return wrapper
-
-
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.wait_for_db()
 
-    @log_db_wait
     def wait_for_db(self):
+        self.stdout.write("Waiting for database...")
         db_conn = None
         while not db_conn:
             try:
                 db_conn = connections["default"]
                 db_conn.ensure_connection()
+                self.stdout.write(self.style.SUCCESS("Database available!"))
             except OperationalError:
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 sleep(1)
