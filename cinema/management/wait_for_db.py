e06a15925ef381f15da394f439b9cf5101844ce2
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-10-10 23:24:00 +0300

    second(changed serializers.py, settings.py and wait_for_db.py)

--- cinema/management/wait_for_db.py
+++ cinema/management/wait_for_db.py
@@ -6,15 +6,15 @@ from django.db import connections, OperationalError
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write('Waiting for database...')
+        self.stdout.write("Waiting for database...")
         connection = None
         while not connection:
             try:
                 connection = connections["default"]
-                connection.cursor().execute('SELECT 1')
+                connection.cursor().execute("SELECT 1")
             except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 connection = None
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+        self.stdout.write(self.style.SUCCESS("Database available!"))
