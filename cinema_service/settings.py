e415f8aea627edd41527b65f4b5c3f547523dba3
Olena Zaritska <zaritskaos@gmail.com>
2024-10-11 17:44:21 +0300

    fix flake8

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",") if os.environ.get("ALLOWED_HOSTS") else []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",") \
+    if os.environ.get("ALLOWED_HOSTS") else []
 
 INTERNAL_IPS = [
     "127.0.0.1",
