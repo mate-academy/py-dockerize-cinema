a18245cc3629eeb97c6a4a7d428979e8ba1852ee
Pasha <pavik.varganov@gmail.com>
2024-10-16 12:53:51 +0300

    Solution 2

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -6,15 +6,21 @@ from django.db.utils import OperationalError
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        self.stdout.write("Waiting for database...")
+    def handle(self, *args, **kwargs):
         db_conn = None
+        max_attempts = 5
+        attempts = 0
 
-        while not db_conn:
+        while not db_conn and attempts < max_attempts:
             try:
+                connections["default"].ensure_connection()
                 db_conn = connections["default"]
             except OperationalError:
-                self.stdout.write("Database unavailable, waiting 1 second...")
+                attempts += 1
+                self.stdout.write(f"Database unavailable, waiting 1 second... (Attempt {attempts}/{max_attempts})")
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS("Database available!"))
+        if db_conn:
+            self.stdout.write(self.style.SUCCESS("Database connection established!"))
+        else:
+            self.stdout.write(self.style.ERROR("Database not available after max attempts."))
