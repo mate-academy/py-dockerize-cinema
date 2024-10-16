63f7b24b1b042f00879773e1fcd074fad1f13c89
Pasha <pavik.varganov@gmail.com>
2024-10-16 14:16:46 +0300

    Solution 3

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -1,4 +1,5 @@
 import time
+from os import getenv
 
 from django.core.management.base import BaseCommand
 from django.db import connections
@@ -7,22 +8,7 @@ from django.db.utils import OperationalError
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        db_conn = None
-        max_attempts = 5
-        attempts = 0
-
-        while not db_conn and attempts < max_attempts:
-            try:
-                connections["default"].ensure_connection()
-                db_conn = connections["default"]
-            except OperationalError:
-                attempts += 1
-                self.stdout.write(
-                    f"Database unavailable, waiting 1 second... "
-                    f"(Attempt {attempts}/{max_attempts})"
-                )
-                time.sleep(1)
-
+        db_conn = self.establish_db_connection()
         if db_conn:
             self.stdout.write(self.style.SUCCESS(
                 "Database connection established!"
@@ -31,3 +17,25 @@ class Command(BaseCommand):
             self.stdout.write(self.style.ERROR(
                 "Database not available after max attempts."
             ))
+
+    def establish_db_connection(self):
+        max_attempts = int(getenv("MAX_ATTEMPTS"))
+        for attempt in range(1, max_attempts + 1):
+            if self.try_connection():
+                return connections["default"]
+            self.log_connection_attempt(attempt, max_attempts)
+            time.sleep(1)
+        return None
+
+    def try_connection(self):
+        try:
+            connections["default"].ensure_connection()
+            return True
+        except OperationalError:
+            return False
+
+    def log_connection_attempt(self, attempt, max_attempts):
+        self.stdout.write(
+            f"Database unavailable, waiting 1 second... "
+            f"(Attempt {attempt}/{max_attempts})"
+        )
--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") != "False"
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1"
+]
 
 INTERNAL_IPS = [
     "127.0.0.1",
