f7ff458c770188980f78f0206e95d8b5d15a7983
popeye88 <aleksei.taran@gmail.com>
2024-12-13 19:44:57 +0200

    solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from cinema.models import (
-    Genre,
     Actor,
     CinemaHall,
+    Genre,
     Movie,
     MovieSession,
-    Ticket,
     Order,
+    Ticket,
 )
 
 
@@ -114,10 +114,10 @@ class TicketSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         data = super(TicketSerializer, self).validate(attrs=attrs)
         Ticket.validate_ticket(
-            attrs["row"], 
-            attrs["seat"], 
-            attrs["movie_session"].cinema_hall, 
-            ValidationError
+            attrs["row"],
+            attrs["seat"],
+            attrs["movie_session"].cinema_hall,
+            ValidationError,
         )
         return data
 
