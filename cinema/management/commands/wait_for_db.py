eeae088b77fc3e3551e97ad5fc819fd64fca67c3
user <nikitaskrypets@gmail.com>
2024-12-19 18:43:01 +0200

    solved permission problems and add wait for db command

---
+++ cinema/management/commands/wait_for_db.py
@@ -0,0 +1,16 @@
+import time
+
+from django.core.management.base import BaseCommand
+from django.db import connections, OperationalError
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        while True:
+            try:
+                connections['default'].ensure_connection()
+                break
+                print("Server loading...")
+            except OperationalError:
+                time.sleep(1)
