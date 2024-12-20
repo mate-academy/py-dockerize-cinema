f53b7c244f6bb64c48016a63661ca8e16a68f864
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-20 22:38:18 +0200

    Solution fix: flake8

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -5,16 +5,16 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Wait for the database to be available'
+    help = "Wait for the database to be available"
 
     def handle(self, *args, **options):
-        self.stdout.write('Waiting for database...')
+        self.stdout.write("Waiting for database...")
         db_conn = None
         while not db_conn:
             try:
-                db_conn = connections['default']
+                db_conn = connections["default"]
                 db_conn.cursor()
             except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+        self.stdout.write(self.style.SUCCESS("Database available!"))
