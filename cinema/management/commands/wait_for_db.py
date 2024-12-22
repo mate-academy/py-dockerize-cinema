a63019f5059ccccb2047a4bc84dabbd34209b826
Alex <delete.rap.stupid@gmail.com>
2024-12-22 20:59:33 +0100

    update flake8 errors

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -5,6 +5,7 @@ from django.core.management import BaseCommand
 from django.db import connections
 from django.db.utils import OperationalError
 
+
 class Command(BaseCommand):
 
     @backoff.on_exception(
@@ -15,7 +16,6 @@ class Command(BaseCommand):
     def connect(self):
         connections["default"].ensure_connection()
         self.stdout.write(self.style.SUCCESS("Connected successfully!"))
-
 
     def handle(self, *args, **kwargs):
         self.stdout.write("Connecting to database...")
