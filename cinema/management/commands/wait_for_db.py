4e08208d5f080a589d97eebcd0a838decfd89c4c
Marharyta Zazulia <zazmargo@gmail.com>
2024-12-26 19:00:32 -0300

    fix bugs flake8 y repushing some files

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
