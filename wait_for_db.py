72b9da0a052a1ee01747be2741697165c6aad5ba
Oleksandr Barabanov <imtence@icloud.com>
2025-02-20 01:02:42 +0200

    Solution

--- wait_for_db.py
+++ wait_for_db.py
@@ -8,6 +8,7 @@ from django.db.utils import OperationalError
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cinema_service.settings")
 django.setup()
 
+
 def wait_for_db():
     print("Очікування бази даних...")
     db_conn = None
@@ -21,6 +22,7 @@ def wait_for_db():
         except OperationalError:
             print("База даних ще не готова, чекаємо 2 секунди...")
             time.sleep(2)
+
 
 if __name__ == "__main__":
     wait_for_db()
