9ebbc933297d00040452ad56f2f598aeac04d920
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-02-19 17:04:23 +0200

    docker compose

---
+++ cinema/management/commands/wait_for_db.py
@@ -0,0 +1,28 @@
+import time
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    help_message = "Waiting for db."
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write("⏳ Waiting for db...")
+        db_conn = None
+        retries = 10
+
+        while not db_conn and retries > 0:
+            try:
+                db_conn = connections["default"]
+                db_conn.cursor()
+            except OperationalError:
+                retries -= 1
+                self.stdout.write("🚧 DB is not available. Retry in 3 sec...")
+                time.sleep(3)
+
+        if not db_conn:
+            self.stderr.write("❌ Failed to connect to db!")
+            exit(1)
+
+        self.stdout.write(self.style.SUCCESS("✅ DB is available!"))
