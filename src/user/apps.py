4c472b21459967519ecbcbcacc1f7a441e4e3978
Ihor Halytskiy <galytskiyihor@gmail.com>
2025-02-20 13:45:17 +0200

    develop

--- src/user/apps.py
+++ src/user/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CinemaConfig(AppConfig):
+class UserConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "cinema"
+    name = "src.user"
