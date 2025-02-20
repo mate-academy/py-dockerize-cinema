c6ab7b5d1aa6bf3e066d66ce5229897683e598ac
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-02-20 11:54:40 +0200

    solution

--- src/cinema/views.py
+++ src/cinema/views.py
@@ -10,7 +10,8 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from src.cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
+from src.cinema.models import (Genre, Actor,
+                               CinemaHall, Movie, MovieSession, Order)
 from src.cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
 
 from src.cinema.serializers import (
