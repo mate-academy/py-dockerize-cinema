71efb7d45632fda92c7443a1ffedcce82d68dc59
Глеб Дубинин <gleb1395@gmail.com>
2025-03-03 21:06:20 +0200

    Solution

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM python:3.11.11-slim
 
-WORKDIR cinema/
+WORKDIR /cinema/
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
+RUN mkdir -p /files/media
+
 COPY . .
 
-RUN mkdir -p /files/media
 #CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
 
 RUN adduser \
---
+++ cinema/management/commands/wait_for_db.py
@@ -0,0 +1,20 @@
+from time import sleep
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for the database...")
+        db_up = False
+        while not db_up:
+            try:
+                connection = connections['default']
+                connection.cursor()
+                db_up = True
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting 5 seconds...")
+                sleep(5)
+        self.stdout.write(self.style.SUCCESS("The database is available!"))
