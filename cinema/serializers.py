d6146662f6871581bf49a23deaf95352638e4e39
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-24 16:19:55 +0300

    serializers.py

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -111,7 +111,15 @@ class MovieSessionListSerializer(MovieSessionSerializer):
 
 
 class TicketSerializer(serializers.ModelSerializer):
-
+    def validate(self, attrs):
+        data = super(TicketSerializer, self).validate(attrs=attrs)
+        Ticket.validate_ticket(
+            attrs["row"],
+            attrs["seat"],
+            attrs["movie_session"].cinema_hall,
+            ValidationError
+        )
+        return data
 
     class Meta:
         model = Ticket
