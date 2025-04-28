a60907221e5f1409f6c6440e34cdbc0681404a0d
Ppllupold <kotnazar2005@gmail.com>
2025-04-28 17:58:53 +0300

    done

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM python:3.11-alpine
+
+LABEL maintainer="kotnazarr2005@gmail.com"
+
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY . .
+RUN mkdir -p /vol/web/media
+
+
+
+RUN adduser \
+        --disabled-password \
+        --no-create-home\
+        django-user
+
+RUN chown -R django-user:django-user /files/media
+RUN chmod -R 755 /files/media
+
+USER django-user
---
+++ cinema/commands/wait_for_db.py
@@ -0,0 +1,27 @@
+import os
+import time
+import psycopg2
+from psycopg2 import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while db_up is False:
+            try:
+                psycopg2.connect(
+                    dbname=os.environ.get("POSTGRES_DB"),
+                    user=os.environ.get("POSTGRES_USER"),
+                    password=os.environ.get("POSTGRES_PASSWORD"),
+                    host=os.environ.get("DB_HOST"),
+                    port=os.environ.get("DB_PORT", "5432"),
+                )
+                db_up = True
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
