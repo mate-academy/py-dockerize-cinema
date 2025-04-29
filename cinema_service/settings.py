bfecae3c0de96e18c82425708be970e8f5de9c3a
Andrew <akolomiec982@gmail.com>
2025-04-30 00:52:35 +0300

    Modified

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": os.environ["POSTGRES_PORT"],
+        "NAME": os.environ.get("POSTGRES_DB", "cinema"),
+        "USER": os.environ.get("POSTGRES_USER", "cinema_user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD","cinema_password"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
