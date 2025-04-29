3b9b545235ff9ac21a6638f247411657cd388455
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-04-29 15:20:00 +0300

    Fix media file serving in DEBUG mode

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
         name="redoc",
     ),
     path("__debug__/", include("debug_toolbar.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
