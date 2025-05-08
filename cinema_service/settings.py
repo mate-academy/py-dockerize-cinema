201cf472d70591f5d9bc615db7b766359d949ee5
yet92 <zyzy92187@gmail.com>
2025-05-08 20:29:59 +0300

    fix: media_volume now mounted to cinema app & change MEDIA_ROOT value

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -140,7 +140,7 @@ USE_TZ = False
 STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "/files/media/"
+MEDIA_ROOT = BASE_DIR / "files" / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
