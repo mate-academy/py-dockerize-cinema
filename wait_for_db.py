f356d8c7d5df20eb4bf5332fd84e38b9715b4fe9
RuslanTopal <baranotik15@gmail.com>
2025-05-08 10:56:22 +0000

    fix flake8

--- wait_for_db.py
+++ wait_for_db.py
@@ -2,14 +2,16 @@ import time
 from django.db import connections
 from django.db.utils import OperationalError
 
+
 def wait_for_db():
     db_conn = None
     while db_conn is None:
         try:
-            db_conn = connections['default']
+            db_conn = connections["default"]
         except OperationalError:
             print("Database unavailable, waiting 1 second...")
             time.sleep(1)
+
 
 if __name__ == "__main__":
     wait_for_db()
