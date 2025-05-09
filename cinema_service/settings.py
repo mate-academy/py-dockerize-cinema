92f1e3ef7191f6987f687bcc5cc2f1516dafc97e
AndyStar <andy_star@ukr.net>
2025-05-09 11:09:25 +0300

    AI mentor issues fixed

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
+        "NAME": os.environ.get("POSTGRES_DB", "cinema"),
+        "USER": os.environ.get("POSTGRES_USER", "cinema"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "cinema"),
+        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
