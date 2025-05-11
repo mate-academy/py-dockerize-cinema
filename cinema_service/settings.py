e7e893ddc43fb65ad02a0cdebf5c95c585ac3eff
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-05-11 15:38:37 +0300

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -97,12 +97,6 @@ DATABASES = {
 }
 
 
-STATIC_URL = "/static/"
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "files" / "media"
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -142,10 +136,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = "/static/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "files" / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
