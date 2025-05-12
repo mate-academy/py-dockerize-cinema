c6e0c09bfa9593109c63038ab15605cb19269ea0
Oneyura <hryn.yuri@gmail.com>
2025-05-12 20:57:20 +0300

    fix flake8

--- management/commands/wait_for_db.py
+++ management/commands/wait_for_db.py
@@ -1,4 +1,4 @@
-import time
+import time as time_module
 from django.core.management.base import BaseCommand
 from django.db import connections
 from django.db.utils import OperationalError
@@ -7,14 +7,14 @@ from django.db.utils import OperationalError
 class Command(BaseCommand):
     help = "Waits for database to be available"
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **kwargs):
         self.stdout.write("Waiting for database...")
-        db_conn = None
-        while not db_conn:
+        connected = False
+        while not connected:
             try:
-                db_conn = connections["default"]
-                db_conn.cursor()
+                connections["default"].cursor()
+                connected = True
             except OperationalError:
                 self.stdout.write("Database unavailable, waiting 1 second...")
-                time.sleep(1)
+                time_module.sleep(1)
         self.stdout.write(self.style.SUCCESS("Database available!"))
