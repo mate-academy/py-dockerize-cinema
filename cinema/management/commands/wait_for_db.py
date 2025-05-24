94f90c6ccd749ef31678a9c7dc008337eceffcf9
Sviatoslav <djsv91@gmail.com>
2025-05-24 19:46:40 +0300

    Solution

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -7,12 +7,15 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         success_connection = None
-        while not success_connection:
+        max_attempt = 30
+        attempt = 1
+        while not success_connection and attempt <= max_attempt:
             try:
                 success_connection = connections["default"]
                 success_connection.cursor()
             except OperationalError:
                 time.sleep(1)
+                attempt += 1
             finally:
                 if success_connection:
                     success_connection.close()
