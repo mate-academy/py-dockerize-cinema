03a2b725c94e87ebaa72831d159970ea070c9b80
lyaskov <lyaskov1@gmail.com>
2025-06-12 20:44:20 +0300

    Convert single quoter to double

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -5,24 +5,24 @@ from django.db.utils import OperationalError
 
 
 class Command(BaseCommand):
-    description = 'Waits for the database to be available.'
+    description = "Waits for the database to be available."
 
     def handle(self, *args, **options):
-        self.stdout.write('⏳ Waiting for database...')
+        self.stdout.write("⏳ Waiting for database...")
         db_conn = None
         retries = 0
 
         while not db_conn:
             try:
-                db_conn = connections['default']
+                db_conn = connections["default"]
                 db_conn.cursor()
             except OperationalError:
                 retries += 1
                 self.stdout.write(
                     self.style.WARNING(
-                        f'⛔ DB unavailable, retrying... ({retries})'
+                        f"⛔ DB unavailable, retrying... ({retries})"
                     )
                 )
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('✅ Database is available!'))
+        self.stdout.write(self.style.SUCCESS("✅ Database is available!"))
